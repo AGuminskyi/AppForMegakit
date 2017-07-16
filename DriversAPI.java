8706bcef787bb6dee3610071f1deffaa06f147b6
Artem Huminskiy <huminskyi1325@gmail.com>
2017-07-16 16:25:21 +0300

    Add DELETE and GET(one driver) methods

--- DriversAPI.java
+++ DriversAPI.java
@@ -3,7 +3,9 @@ package com.android.huminskiy1325.appformegakit;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by cubru on 15.07.2017.
@@ -14,4 +16,12 @@ public interface DriversAPI {
     @GET("drivers/")
     Call<List<DriversModel>> getDriversList();
 
+    @GET("drivers/{id}")
+    Call<DriversModel> getDriver(@Path("id") String id);
+
+    @DELETE("drivers/{id}")
+    Call<Object> deleteDriver(@Path("id") String id);
+
+    @DELETE("cars/{id}")
+    Call<Object> deleteCar(@Path("id") int id);
 }
--- MainFragment.java
+++ MainFragment.java
@@ -8,11 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,22 +27,27 @@ import retrofit2.Response;
 
 public class MainFragment extends Fragment {
     public DriversAPI driversAPI;
-    public List<DriversModel> modelList;
+    public static DriversModel modelDriver = null;
 
     Button getAllBtn;
+    Button getOneBtn;
+    Button deleteOneBtn;
     TextView informationTV;
+    EditText getOneET;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         driversAPI = ConnectionData.getInstance(getActivity().getApplicationContext());
-        modelList = new ArrayList<>();
+
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         getAllBtn = (Button) view.findViewById(R.id.get_all_btn);
-        informationTV = (TextView)view.findViewById(R.id.informationTV);
-
+        getOneBtn = (Button) view.findViewById(R.id.get_one_btn);
+        deleteOneBtn = (Button) view.findViewById(R.id.delete_one_btn);
+        informationTV = (TextView) view.findViewById(R.id.informationTV);
+        getOneET = (EditText) view.findViewById(R.id.get_id_ET);
 
 
         getAllBtn.setOnClickListener(new View.OnClickListener() {
@@ -48,8 +56,12 @@ public class MainFragment extends Fragment {
                 driversAPI.getDriversList().enqueue(new Callback<List<DriversModel>>() {
                     @Override
                     public void onResponse(Call<List<DriversModel>> call, Response<List<DriversModel>> response) {
-                        modelList.addAll(response.body());
-                        informationTV.setText(modelList.toString());
+                        if (response.isSuccessful()) {
+                            List<DriversModel> modelList = response.body();
+                            informationTV.setText(modelList.toString());
+                        } else {
+
+                        }
                     }
 
                     @Override
@@ -60,6 +72,76 @@ public class MainFragment extends Fragment {
                 });
             }
         });
+
+        getOneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                driversAPI.getDriver(getOneET.getText().toString()).enqueue(new Callback<DriversModel>() {
+                    @Override
+                    public void onResponse(Call<DriversModel> call, Response<DriversModel> response) {
+                        if (response.isSuccessful()) {
+                            modelDriver = response.body();
+                            informationTV.setText(modelDriver.toString());
+                        } else {
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<DriversModel> call, Throwable t) {
+                        Log.d("NEW_TAG", t.getMessage());
+                        Toast.makeText(getActivity(), "An error occurred during networking", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        deleteOneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                carsDeleted();
+                driversAPI.deleteDriver(getOneET.getText().toString().trim()).enqueue(new Callback<Object>() {
+                    @Override
+                    public void onResponse(Call<Object> call, Response<Object> response) {
+                        if (response.isSuccessful()) {
+                            informationTV.setText("Deleted");
+                        } else {
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Object> call, Throwable t) {
+                        Log.d("NEW_TAG", t.getMessage());
+                        Toast.makeText(getActivity(), "An error occurred during networking",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
         return view;
+    }
+
+    private void carsDeleted() {
+        if (modelDriver == null) {
+            Toast.makeText(getActivity(), "At first you should to know Drivers List",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            List<Car> carList = modelDriver.getCars();
+            for (Car car : carList) {
+                driversAPI.deleteCar(car.getId()).enqueue(new Callback<Object>() {
+                    @Override
+                    public void onResponse(Call<Object> call, Response<Object> response) {
+                        Toast.makeText(getActivity(), "Car's deleted", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Object> call, Throwable t) {
+
+                    }
+                });
+            }
+        }
     }
 }
